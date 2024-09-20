9371c4e85257ffd1daac5e0461eebebebfd46149
Vstrel <strelnikova.vs@phystech.edu>
2024-09-20 15:58:23 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,12 @@
-def fib(a):
-    if a==1 or a==0:
-        return(1)
-    return(fib(a-1)+fib(a-2))
-a=int(input())
-print(fib(a))
+def fib(n, cache):
+    if n==1 or n==0:
+        cache[n]=1
+        return 1
+    if cache[n]!=0:
+        return cache[n]
+    cache[n] = fib(n-1, cache) + fib(n-2, cache)
+    return cache[n]
+
+n=105
+cache = [0 for i in range(n+1)]
+print(fib()n,cache)
